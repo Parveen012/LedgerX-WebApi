4a724d8aae6fd71305dd6368c7812913aed1170a
Parveen Verma <vparveen012@gmail.com>
2026-03-09 15:22:57 +0530

    Add project files.

---
+++ Domain/ShopSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain;
+
+public class ShopSettings
+{
+    [Key]
+    public int Id { get; set; }
+    public string ShopName { get; set; }
+    public string OwnerName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string? GstNumber { get; set; }
+    public int EmployeeId { get; set; }
+
+
+}
